f5f3dce52deee6c0af5dc467a3ea9556c29ec2aa
Twmcy <156540799+Twmcy@users.noreply.github.com>
2024-01-22 12:38:53 +0800

    Add files via upload

--- app/src/main/java/com/projectkr/shell/permissions/CheckRootStatus.kt
+++ app/src/main/java/com/projectkr/shell/permissions/CheckRootStatus.kt
@@ -13,15 +13,15 @@ import com.projectkr.shell.R
 import kotlin.system.exitProcess
 
 /**
- * 检查获取 root 权限
+ * 检查获取root权限
  * Created by helloklf on 2017/6/3.
  */
-class CheckRootStatus(var context: Context, private var next: Runnable? = null) {
-    private var myHandler: Handler = Handler(Looper.getMainLooper())
 
-    private var therad: Thread? = null
+public class CheckRootStatus(var context: Context, private var next: Runnable? = null) {
+    var myHandler: Handler = Handler(Looper.getMainLooper())
 
-    fun forceGetRoot() {
+    var therad: Thread? = null
+    public fun forceGetRoot() {
         if (lastCheckResult) {
             if (next != null) {
                 myHandler.post(next)
@@ -43,9 +43,25 @@ class CheckRootStatus(var context: Context, private var next: Runnable? = null) 
                 } else {
                     myHandler.post {
                         KeepShellPublic.tryExit()
+                        val builder = AlertDialog.Builder(context) { _, _ ->
+                                    KeepShellPublic.tryExit()
+                                    if (therad != null && therad!!.isAlive && !therad!!.isInterrupted) {
+                                        therad!!.interrupt()
+                                        therad = null
+                                    }
+                                    forceGetRoot()
+                                }{ _, _ ->
+                                    exitProcess(0)
+                                    //android.os.Process.killProcess(android.os.Process.myPid())
+                                }
                         if (context.resources.getBoolean(R.bool.force_root) != true) {
-                            showAlertDialog()
+                            builder.setNeutralButton { _, _ ->
+                                if (next != null) {
+                                    myHandler.post(next)
+                                }
+                            }
                         }
+                        DialogHelper.animDialog(builder).setCancelable(false)
                     }
                 }
             }
@@ -56,53 +72,57 @@ class CheckRootStatus(var context: Context, private var next: Runnable? = null) 
                 if (!completed) {
                     KeepShellPublic.tryExit()
                     myHandler.post {
-                        if (context.resources.getBoolean(R.bool.force_root) != true) {
-                            showTimeoutAlertDialog()
-                        }
+                        DialogHelper.confirm(context,
+                        context.getString(R.string.error_root),
+                        context.getString(R.string.error_su_timeout),
+                        null,
+                        DialogHelper.DialogButton(context.getString(R.string.btn_retry), {
+                            if (therad != null && therad!!.isAlive && !therad!!.isInterrupted) {
+                                therad!!.interrupt()
+                                therad = null
+                            }
+                            forceGetRoot()
+                        }),
+                        DialogHelper.DialogButton(context.getString(R.string.btn_exit), {
+                            exitProcess(0)
+                        }))
                     }
                 }
             }).start()
         }
     }
 
-    private fun showAlertDialog() {
-        val builder = AlertDialog.Builder(context)
-                .setTitle(R.string.error_root)
-                .setPositiveButton(R.string.btn_retry) { _, _ ->
-                    KeepShellPublic.tryExit()
-                    if (therad != null && therad!!.isAlive && !therad!!.isInterrupted) {
-                        therad!!.interrupt()
-                        therad = null
-                    }
-                    forceGetRoot()
-                }
-                .setNegativeButton(R.string.btn_exit) { _, _ ->
-                    exitProcess(0)
-                }
-        DialogHelper.animDialog(builder).setCancelable(false)
-    }
-
-    private fun showTimeoutAlertDialog() {
-        DialogHelper.confirm(context,
-            context.getString(R.string.error_root),
-            context.getString(R.string.error_su_timeout),
-            null,
-            DialogHelper.DialogButton(context.getString(R.string.btn_retry), {
-                if (therad != null && therad!!.isAlive && !therad!!.isInterrupted) {
-                    therad!!.interrupt()
-                    therad = null
-                }
-                forceGetRoot()
-            }),
-            DialogHelper.DialogButton(context.getString(R.string.btn_exit), {
-                exitProcess(0)
-            }))
-    }
-
     companion object {
         private var rootStatus = false
+        private fun checkPermission(context: Context, permission: String): Boolean = PermissionChecker.checkSelfPermission(context, permission) == PermissionChecker.PERMISSION_GRANTED
+        fun grantPermission(context: Context) {
+            val cmds = StringBuilder()
+            /*
+            // 必需的权限
+            val requiredPermission = arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            requiredPermission.forEach {
+                if (!checkPermission(context, it)) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        val option = it.substring("android.permission.".length)
+                        cmds.append("appops set ${context.packageName} ${option} allow\n")
+                    }
+                    cmds.append("pm grant ${context.packageName} $it\n")
+                }
+            }
+            */
 
-        // 最后的 ROOT 检测结果
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!checkPermission(context, Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)) {
+                    cmds.append("dumpsys deviceidle whitelist +${context.packageName};\n")
+                }
+            }
+            KeepShellPublic.doCmdSync(cmds.toString())
+        }
+
+        // 最后的ROOT检测结果
         val lastCheckResult: Boolean
             get() {
                 return rootStatus
